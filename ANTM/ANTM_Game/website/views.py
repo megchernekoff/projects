7b04743e1f42c84e701058bac880acc8b2a8b5a2
megchernekoff <megchernekoff@proton.me>
2023-04-02 22:09:03 -0700

    correct autofill

--- ANTM/ANTM_Game/website/views.py
+++ ANTM/ANTM_Game/website/views.py
@@ -21,28 +21,25 @@ def index(request):
         form = CycleForm()
         return render(request, 'website/index.html', {"form":form})
 
+
 def redir_func(request, wordbank, id):
     if wordbank == 'False':
         wordbank = ''
     my_list_shuff =  [c.name for c in Contestants.objects.raw("select * from website_contestants where cycle = 'Cycle {}';".format(id))]
     my_list = my_list_shuff.copy()
-    print(my_list)
+    options_list = ['option{}'.format(num) for num in range(len(my_list))]
     random.shuffle(my_list_shuff)
-    print(my_list)
-    print('')
 
     if request.method == 'POST':
         result_list = []
         for f in my_list:
             form = request.POST.get(f)
             result_list.append(form)
-        print(my_list)
-        print(result_list)
-        return redirect('/results', {'result_list':result_list})
+        return redirect('/results', {'my_list': my_list, 'options_list':options_list, 'result_list':result_list})
 
 
     else:
-        return render(request, 'website/redir.html', {"cyc_id":id, "wordbank":wordbank, "my_list":my_list, "my_list_shuff":my_list_shuff})
+        return render(request, 'website/redir.html', {"cyc_id":id, "wordbank":wordbank, 'list_opt':zip(my_list,options_list), "my_list":my_list})
 
 
 def results_func(request):
